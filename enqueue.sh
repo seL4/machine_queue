7b934710a5445e6cc0bc0d1dfee559bb51e76769
Adrian Danis <Adrian.Danis@nicta.com.au>
2013-09-13 16:10:01 +1000

    Add sabre3 to bamboo notifications

--- enqueue.sh
+++ enqueue.sh
@@ -227,6 +227,8 @@ elif [ "$system" = "vtd2" ]; then
     curl --user admin:ertos -X POST -d 'JOB1&ExecuteAllStages' 'http://saison.keg.ertos.in.nicta.com.au:8085/rest/api/latest/queue/TOOLS-RUNQUEUEDVTD2JOBS'
 elif [ "$system" = "sabre" ]; then
     curl --user admin:ertos -X POST -d 'JOB1&ExecuteAllStages' 'http://saison.keg.ertos.in.nicta.com.au:8085/rest/api/latest/queue/TOOLS-RUNQUEUEDSABREJOBS'
+elif [ "$system" = "sabre3" ]; then
+    curl --user admin:ertos -X POST -d 'JOB1&ExecuteAllStages' 'http://saison.keg.ertos.in.nicta.com.au:8085/rest/api/latest/queue/TOOLS-RUNQUEUEDSABRE3JOBS'
 fi
 
 # Setup a trap handler that will remove this job and notify the server if we try and quit
