dc2d42705a40fabebf0711da4064aaa372a7ee4b
Adrian Danis <Adrian.Danis@nicta.com.au>
2013-09-18 12:08:46 +1000

    Propagate error codes from server reboot script to client enqueue script

--- common_run_1file.sh
+++ common_run_1file.sh
@@ -31,6 +31,8 @@ if [ "$ci" = "-r" ]; then
     # why? I have no idea, but if this is executed it will leave console sessions open
     # all over the place. We also redirect the output to 3 different places, which
     # is possibly a little insane
+    # There is also no error handling of the console command failing and all the logic will
+    # probably get very fucked up / confused if that happens
     setsid bash -c "console -f $console | { while read line; do echo \$line; echo \$line >> \"$BASE/$jobid/output.pipe\"; echo \$line >> $LOG_FILE; done; }" &
     pid=$!
     while read input
@@ -38,11 +40,15 @@ if [ "$ci" = "-r" ]; then
         echo $input >> "$BASE/$jobid/console.pipe"
     done < $INPUT
     kill -- -$pid
+    # Reservations cannot fail as it doesn't make sense
+    echo "0" > "$BASE/$jobid/return"
 elif [ "$ci" = "-c" ] ; then
     $REBOOT_CMD -c "$COMPLETION_TXT" -t -1 -l "$LOG_FILE" -k "$BASE/$jobid/file0" < $INPUT | tee "$BASE/$jobid/output.pipe"
+    echo ${PIPESTATUS[0]} > "$BASE/$jobid/return"
 else
     # Need to special case -c and -i because you cannot specify a blank command line option after -i
     $REBOOT_CMD -i -t -1 -l "$LOG_FILE" -k "$BASE/$jobid/file0" < $INPUT | tee "$BASE/$jobid/output.pipe"
+    echo ${PIPESTATUS[0]} > "$BASE/$jobid/return"
 fi
 
 echo "Success" > "$BASE/$jobid/output.pipe"
