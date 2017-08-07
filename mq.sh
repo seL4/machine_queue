07ff6c25bebc49cc2cf26d5c846950dc9213ff41
Luke Mondy <luke.mondy@data61.csiro.au>
2017-08-07 14:32:50 +1000

    Passed mq through shellcheck.net and fixed potential bugs

--- mq.sh
+++ mq.sh
@@ -4,10 +4,10 @@
 pushd . > /dev/null
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
-  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+    while([ -h "${SCRIPT_PATH}" ]) do cd "$(dirname "$SCRIPT_PATH")" || exit; SCRIPT_PATH=$(readlink "${SCRIPT_PATH}"); done
 fi
-cd `dirname ${SCRIPT_PATH}` > /dev/null
-SCRIPT_PATH=`pwd`;
+cd "$(dirname "${SCRIPT_PATH}")" || exit > /dev/null
+SCRIPT_PATH=$(pwd);
 popd  > /dev/null
 
 # Include common elements
