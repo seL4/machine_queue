d9a0bdd901b1b07d3488e9b53d29a7cccf0c7eb9
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-08-07 18:30:11 +1000

    bash completion for mq.sh
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ bash_mq_completion.sh
@@ -0,0 +1,53 @@
+# bash completion for mq.sh
+# source this file to use: . ./bash_mq_completion.sh
+
+_mq_systems() {
+  mq.sh system-tsv | cut -f 1 | tail -n +2
+  mq.sh pool-tsv | cut -f 1
+}
+
+_mq_completion() {
+  local cur="${COMP_WORDS[$COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq "1" ]; then
+    COMPREPLY=($(compgen -W "run sem systems system-tsv pool-tsv" -- "${cur}"))
+  else
+    local prev="${COMP_WORDS[$(( $COMP_CWORD - 1 ))]}"
+    case "${COMP_WORDS[1]}" in
+      sem)
+        if [ "$COMP_CWORD" -eq "2" ]; then
+          COMPREPLY=($(compgen -W "-signal -wait -info -mr-info -cancel" -- "${cur}"))
+        elif [ "$COMP_CWORD" -eq "3" ]; then
+          COMPREPLY=($(compgen -W "$(_mq_systems)" -- "${cur}"))
+        elif [ "$COMP_CWORD" -eq "4" ]; then
+          COMPREPLY=($(compgen -W "-k -f" -- "${cur}"))
+        fi
+      ;;
+      run)
+        local mq_run_flags="-r -n -a -c -e -d -k -l -s -f -w -t"
+        if [ "$COMP_CWORD" -eq "2" ]; then
+          COMPREPLY=($(compgen -W "${mq_run_flags}" -- "${cur}"))
+        else
+          case "$prev" in
+            '-s')
+              COMPREPLY=($(compgen -W "$(_mq_systems)" -- "${cur}"))
+            ;;
+            '-k'|'-c'|'-e'|'-d'|'-k'|'-l'|'-w'|'-t')
+            ;;
+            '-f')
+              COMPREPLY=($(compgen -f -- "${cur}"))
+            ;;
+            *)
+              COMPREPLY=($(compgen -W "${mq_run_flags}" -- "${cur}"))
+            ;;
+          esac
+        fi
+      ;;
+      *)
+      ;;
+    esac
+  fi
+
+}
+
+complete -F _mq_completion mq.sh
