4c513e3207b6b7476399e1ab07604de842990af7
Peter Chubb <Peter.Chubb@data61.csiro.au>
2021-09-20 14:14:39 +1000

    Warn lock-holders
    Add a script to warn holders of locks after four hours.
    
    Signed-off-by: Peter Chubb <peter.chubb@unsw.edu.au>

---
+++ warn_holders.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+now=`date +'%s'`
+fourHrs=14400
+
+warn() {
+    mail -f 'LockBot@trustworthy.systems' $1@trustworthy.systems -s "Lock for $2 held for a long time" <<EOF
+    Hi $1,
+    You have now held the machine queue lock for $2 for 
+    over `expr $diff / 3600` hours.  That's OK if you're still using $2, 
+    but otherwise,  please release the lock 
+
+    The Lockbot will remind you every four hours ...
+    Your friendly lockbot.
+EOF
+}
+
+mq.sh sem dumpall |
+    while read system lockstate user day time zone key
+    do
+	secs=$(date -d "$day $time $zone" +'%s')
+	diff=$(expr $now - $secs)
+	[ $diff -gt $fourHrs ] && warn "$user" "$system" "$diff"
+    done
+
+	
